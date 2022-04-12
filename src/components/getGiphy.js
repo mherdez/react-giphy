593df5d9f9f2419b1b143e1b526300f636c065ca
Manuel Herdez <herdez@gmail.com>
2022-04-11 19:35:45 -0500

    getGiphy

--- src/components/getGiphy.js
+++ src/components/getGiphy.js
@@ -1,1 +1,8 @@
-// https://api.giphy.com/v1/gifs/search?api_key=qASb90GT51gqt90LmBe6KxHqOuZdmymn&q=cats&limit=25&offset=0&rating=g&lang=es
+export const getGiphy = async (category) => {
+	const url = `https://api.giphy.com/v1/gifs/search?api_key=qASb90GT51gqt90LmBe6KxHqOuZdmymn&q=${category}&limit=25&offset=0&rating=g&lang=es`;
+
+	const resp = await fetch(url);
+	const { data } = await resp.json();
+	// console.log(data);
+	return data;
+};
